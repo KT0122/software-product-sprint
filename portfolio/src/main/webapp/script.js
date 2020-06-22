5b555d6384e99e06c000cf9f2da14a967f1fa6bd
Khalil <KhalilTavarez@gmail.com>
2020-06-22 11:22:47 -0400

    Added a few new things, Mainly a marker to the map to point to rit and explenation of the importance of said marker  along with a dark mode button to make it easier for me to see and for others that prefer dark mode.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -45,8 +45,101 @@ function addDataToDom(data) {
     dataContainer.innerText = data;
 }
 
+function darkModeFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
 function createMap() {
-  const map = new google.maps.Map(
+    var RitCords = {lat: 43.0845, lng: -77.6749};
+    //var SantiagoCords = {lat: 19.4792, long: -70.6931};
+    //var NyCords = {lat:40.7128, long: -74.0060};
+
+    const map = new google.maps.Map(
       document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
-}x
+      {center: RitCords, zoom: 15, 
+      styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]});
+
+    var RitMarker = new google.maps.Marker({position: RitCords, map: map});
+    //var SantiagoMarker = new google.maps.Marker({position: SantiagoCords, map: map});
+    //var NycMarker = new google.maps.Marker({position: NyCords, map: map});
+}
